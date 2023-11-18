e9e8a06b8e6750330b6e4cd2ba439378b05bb40d
MarcosDalessandro <marcosdalessandroclima@ufpi.edu.br>
2023-11-18 15:37:06 -0300

    trying make the api works in railway

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,13 @@ import { AppService } from './app.service';
 import { StringifyOptions } from 'querystring';
 import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { User } from './users/entities/user.entity';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @IsPublic
   @Get()
   getHello(): string {
     return this.appService.getHello();
