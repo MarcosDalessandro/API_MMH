692c34d7276a5ca088b84e848e842d040bf2a409
MarcosDalessandro <marcosdalessandroclima@ufpi.edu.br>
2023-11-21 16:12:59 -0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
-  //const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000;
 
   // Listen on `port` and 0.0.0.0
-  //app.listen(port, "0.0.0.0", function () {})
+  app.listen(port, "0.0.0.0", function () { })
 }
 
 bootstrap();
